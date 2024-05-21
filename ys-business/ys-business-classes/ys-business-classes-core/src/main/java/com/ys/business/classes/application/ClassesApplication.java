5a7829e42c6bef82c1ad33c825caf9a6756adcfd
wangzx <2099266050@qq.com>
2024-05-21 17:14:50 +0800

    feat: 微服务调用

---
+++ ys-business/ys-business-classes/ys-business-classes-core/src/main/java/com/ys/business/classes/application/ClassesApplication.java
@@ -0,0 +1,15 @@
+package com.ys.business.classes.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 班级服务的启动类
+ */
+@SpringBootApplication
+public class ClassesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClassesApplication.class,args);
+    }
+
+}
