fc96d32a1264aeca5dd85e66d63b75bf79fead5e
wangzx <2099266050@qq.com>
2024-05-21 16:24:55 +0800

    feat: nacos配置

--- ys-business/ys-business-student/ys-business-student-core/src/main/java/com/ys/business/student/application/StuApplication.java
+++ ys-business/ys-business-student/ys-business-student-core/src/main/java/com/ys/business/student/application/StuApplication.java
@@ -1,10 +1,17 @@
 package com.ys.business.student.application;
 
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
 import com.ys.commons.web.apiversion.register.EnableApiVersion;
 import com.ys.commons.web.devicetype.register.EnableDeviceType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableApiVersion
@@ -16,4 +23,6 @@ public class StuApplication {
     public static void main(String[] args) {
         SpringApplication.run(StuApplication.class,args);
     }
+
+
 }
--- ys-commons/ys-commons-web/src/main/java/com/ys/commons/web/config/BaseConfiguration.java
+++ ys-commons/ys-commons-web/src/main/java/com/ys/commons/web/config/BaseConfiguration.java
@@ -1,10 +1,17 @@
 package com.ys.commons.web.config;
 
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
 import com.ys.commons.web.exception.GlobalException;
 import com.ys.commons.web.utils.ApplicationUtils;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * description: Web模块的自动装配的核心配置文件
@@ -63,6 +70,23 @@ public class BaseConfiguration {
     @Configuration
     public static class NacosConfiguration{
 
+        /**
+         * 配置微服务相关元数据信息
+         * @return
+         */
+        @Bean
+        @Primary
+        public NacosDiscoveryProperties getNacosDiscoveryProperties(){
+            NacosDiscoveryProperties nacosDiscoveryProperties = new NacosDiscoveryProperties();
+
+            // 设置相关的元数据信息
+            Map<String, String> map = new HashMap<>();
+            map.put("online.time", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+
+            nacosDiscoveryProperties.setMetadata(map);
+
+            return nacosDiscoveryProperties;
+        }
     }
 
 
